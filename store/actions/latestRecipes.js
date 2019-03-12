2d7a58a3bc3c8fd43dcac428701276986d398fda
karanwadhwa <wadhwa.karan96@gmail.com>
2019-03-13 00:40:34 +0530

    fetchLatestRecipes ActionCreator and Reducer

---
+++ store/actions/latestRecipes.js
@@ -0,0 +1,31 @@
+import {
+  SET_LATEST_RECIPES,
+  SELECT_LATEST_RECIPE,
+  SET_LATEST_LOADING,
+  SET_LATEST_REFRESHING
+} from "./types";
+import API from "../../config/api";
+
+export const setLatestLoading = () => {
+  return {
+    type: SET_LATEST_LOADING
+  };
+};
+
+export const setLatestRefreshing = () => {
+  return {
+    type: SET_LATEST_REFRESHING
+  };
+};
+
+export const fetchLatestRecipes = () => dispatch => {
+  dispatch(setLatestLoading());
+  dispatch(setLatestRefreshing());
+
+  API.get("/latest.php").then(response => {
+    dispatch({
+      type: SET_LATEST_RECIPES,
+      payload: response.data.meals
+    });
+  });
+};
--- store/reducers/Latest.js
+++ store/reducers/Latest.js
@@ -1,10 +1,36 @@
+import {
+  SET_LATEST_RECIPES,
+  SELECT_LATEST_RECIPE,
+  SET_LATEST_LOADING,
+  SET_LATEST_REFRESHING
+} from "../actions/types";
+
 initialState = {
   recipes: [],
-  selectedRecipe: null
+  selectedRecipe: null,
+  loading: true,
+  refreshing: false
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SET_LATEST_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case SET_LATEST_REFRESHING:
+      return {
+        ...state,
+        refreshing: true
+      };
+    case SET_LATEST_RECIPES:
+      return {
+        ...state,
+        recipes: action.payload,
+        loading: false,
+        refreshing: false
+      };
     default:
       return state;
   }
