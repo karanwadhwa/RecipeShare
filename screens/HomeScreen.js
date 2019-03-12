336f37d0a7b9c35db8a78aa2d492556eddf5b82a
karanwadhwa <wadhwa.karan96@gmail.com>
2019-03-13 00:41:27 +0530

    fetch and populate HomeScreen with latest recipes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,25 +1,115 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { connect } from "react-redux";
+import { ActivityIndicator, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Subtitle, Caption, Title } from "@shoutem/ui";
+import Masonry from "react-native-masonry";
+
+import { fetchLatestRecipes } from "../store/actions/latestRecipes";
 
 class HomeScreen extends Component {
   static navigationOptions = {
     title: "Home"
   };
 
-  render() {
+  componentDidMount() {
+    this.props.fetchLatestRecipes();
+  }
+
+  renderLatestRecipes = () => {
+    const { recipes } = this.props;
+    const bricks = recipes.map(recipe => {
+      return {
+        data: recipe,
+        key: recipe.idMeal,
+        uri: recipe.strMealThumb,
+        renderFooter: data => {
+          return (
+            <View
+              style={{
+                backgroundColor: "#FBFBFB",
+                borderColor: "#E4E4E4",
+                borderWidth: 0.5,
+                padding: 10,
+                borderBottomRightRadius: 10,
+                borderBottomLeftRadius: 10
+              }}
+            >
+              <Title>{data.strMeal}</Title>
+              <View styleName="horizontal space-between">
+                <Caption
+                  style={{
+                    color: "#555",
+                    marginTop: 5,
+                    alignSelf: "flex-start"
+                  }}
+                >
+                  {data.strCategory}
+                </Caption>
+                <Caption
+                  style={{
+                    color: "#999",
+                    marginTop: 5,
+                    alignSelf: "flex-start"
+                  }}
+                >
+                  {data.strArea}
+                </Caption>
+              </View>
+            </View>
+          );
+        }
+      };
+    });
+
     return (
-      <View style={styles.container}>
-        <Text> HomeScreen </Text>
-      </View>
+      <Masonry
+        columns={2}
+        bricks={bricks}
+        spacing={2}
+        imageContainerStyle={{
+          borderTopLeftRadius: 10,
+          borderTopRightRadius: 10
+        }}
+      />
     );
+  };
+
+  render() {
+    if (!this.props.recipes[0]) {
+      return (
+        <ActivityIndicator
+          size="large"
+          color="#007AFF"
+          style={styles.ActivityIndicator}
+        />
+      );
+    }
+
+    return <View style={styles.container}>{this.renderLatestRecipes()}</View>;
   }
 }
 
-export default HomeScreen;
+const mapStateToProps = state => {
+  return {
+    loading: state.latest.loading,
+    refreshing: state.latest.refreshing,
+    recipes: state.latest.recipes
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchLatestRecipes }
+)(HomeScreen);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 15
+    padding: 10
+  },
+  ActivityIndicator: {
+    flex: 1,
+    alignSelf: "center",
+    justifyContent: "center"
   }
 });
