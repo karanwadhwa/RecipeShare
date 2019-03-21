de2f68b264fe9ff8c7333beaa4345b56a289682a
karanwadhwa <wadhwa.karan96@gmail.com>
2019-03-21 17:47:12 +0530

    removed redux devtools extension config

--- store/index.js
+++ store/index.js
@@ -4,13 +4,13 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initialState = {};
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
   initialState,
-  composeEnhancers(applyMiddleware(...middleware))
+  applyMiddleware(...middleware)
 );
 
 export default store;
